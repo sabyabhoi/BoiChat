3e8ef998234096871c19f12f49e52bc32a53435e
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2022-02-03 12:48:47 +0530

    Make a basic chat UI

---
+++ pages/components/messageBox.tsx
@@ -0,0 +1,12 @@
+import type { NextComponentType } from 'next';
+
+const MessageBox: NextComponentType = () => {
+  return (
+    <div className='flex flex-row gap-x-1 pb-4'>
+      <textarea placeholder='Enter your message here' className='outline-none flex-grow-[6] bg-secondary-lite resize-none rounded-md'></textarea>
+      <button className='flex-grow bg-red text-secondary font-bold text-lg rounded-md'>Send</button>
+    </div>
+  );
+};
+
+export default MessageBox;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+  theme: {
+		extend: { },
+		colors: {
+			primary: '#ebdbb2',
+			secondary: '#282828',
+			'secondary-lite': '#3c3836',
+			mute: '#928374',
+			red: '#fb4934',
+		}
+  },
+  plugins: [],
+}
